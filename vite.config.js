4277407f761bdac20d573bd30d6470a9d5863552
arkady <arkadychakhalyan@gmail.com>
2024-06-29 21:37:00 +0300

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte()],
+    base: process.env.NODE_ENV === 'production' ? '/wordle/' : '/',
+    plugins: [svelte()],
 })
