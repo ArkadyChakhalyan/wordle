e0f9b3b1d17b1b29f05f8f40816f5566cdccdb7c
arkady <arkadychakhalyan@gmail.com>
2024-06-29 22:00:27 +0300

    gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,16 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
-export default {
-  preprocess: vitePreprocess(),
-}
+const config = {
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            base: '/wordle/'
+        }
+    }
+};
+
+export default config;
