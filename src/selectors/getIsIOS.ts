e6951cced79a5b29d73c8a99fcd79a6c29fdfeb9
arkady <arkadychakhalyan@gmail.com>
2024-06-30 14:01:21 +0300

    minor fixes

--- src/selectors/getIsIOS.ts
+++ src/selectors/getIsIOS.ts
@@ -1,3 +1,4 @@
 export const getIsIOS = (): boolean => {
+    alert(navigator.platform + ' ' +  ['iPod', 'iPhone'].includes(navigator.platform))
     return ['iPod', 'iPhone'].includes(navigator.platform);
 }
